043a816666be6dbdc938c44afb059ccadc20beb3
Manuel <mzamorad95@gmail.com>
2017-11-06 18:03:55 +0100

    Cambios

--- morn.js
+++ morn.js
@@ -1,18 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 //Constante con la lista de comandos disponibles, modificar simpre que se añada o se borre un comando. Separarlos con \n
 const lista = "\nLista de comandos disponibles: \n\n" +
 	"!comandos : Devuelve la lista de comandos disponibles en el bot" 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
 	
+client.on('message', message => {
 	if (message.content === '!comandos') {
     	message.reply(lista);
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
+// Proceso de login inicial del Bot, imprescindible para su funcionamiento
 client.login(process.env.BOT_TOKEN);
