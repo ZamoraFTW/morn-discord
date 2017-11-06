a932fdc247421a47f8ab7401667dfb3b4be90548
Manuel <mzamorad95@gmail.com>
2017-11-06 18:24:44 +0100

    Cambios

--- morn.js
+++ morn.js
@@ -11,5 +11,14 @@ client.on('message', message => {
   	}
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send('¡Bienvenido a Ascent Dragons, ${member}');
+});
+
 // Proceso de login inicial del Bot, imprescindible para su funcionamiento
 client.login(process.env.BOT_TOKEN);
