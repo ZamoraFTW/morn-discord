8149bdc6c87ad1b8edba006963589754a360b664
Manuel <mzamorad95@gmail.com>
2017-11-06 17:43:55 +0100

    Cambios

--- morn.js
+++ morn.js
@@ -1,18 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+//Constante con la lista de comandos disponibles, modificar simpre que se añada o se borre un comando. Separarlos con \n
+const lista = "\nLista de comandos disponibles: \n\n" +
+	"!comandos : Devuelve la lista de comandos disponibles en el bot" 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
 	
-	const lista = "\nLista de comandos disponibles: \n" +
-	"!comandos : Devuelve la lista de comandos disponibles en el bot" 
-	
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
 	if (message.content === '!comandos') {
     	message.reply(lista);
   	}
