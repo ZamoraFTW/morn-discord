5ba060e19aa7cb8769112d6d0b504bca61b1d3dc
Manuel <mzamorad95@gmail.com>
2017-11-07 09:35:43 +0100

    fix

--- morn.js
+++ morn.js
@@ -1,19 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const Twitter = require('twitter');
 //Constante con la lista de comandos disponibles, modificar simpre que se añada o se borre un comando. Separarlos con \n
 const lista = "\nLista de comandos disponibles: \n\n" +
 	"!comandos : Devuelve la lista de comandos disponibles en el bot" 
+
+// Variable para la conexion con la API de Twitter 	
+var tw = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+// Proceso de login inicial del Bot, imprescindible para su funcionamiento
+client.login(process.env.BOT_TOKEN);
 	
 client.on('message', message => {
 	if (message.content === '!comandos') {
     	message.reply(lista);
 	}
-	if (message.content === '!roles') {
-    	message.reply(lista);
+	if (message.content === '!tweets') {
+    	tw.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+			message.reply(tweets);
+		});
   	}
 });
 
-
-// Proceso de login inicial del Bot, imprescindible para su funcionamiento
-client.login(process.env.BOT_TOKEN);
