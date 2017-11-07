cd60e3d351c9c38a9e08c23528628dc6871e5d33
Manuel <mzamorad95@gmail.com>
2017-11-07 16:40:10 +0100

    fix

--- morn.js
+++ morn.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const client = new Discord.Client();
 //Constante con la lista de comandos disponibles, modificar simpre que se añada o se borre un comando. Separarlos con \n
 const lista = "\nLista de comandos disponibles: \n\n" +
-	"!comandos : Devuelve la lista de comandos disponibles en el bot" 
+	"!comandos : Devuelve la lista de comandos disponibles en el bot"
 // Canal de texto de administracion
 const txtAdministracion = process.env.ID_ADMINISTRACION;
 // ID del servidor
 const idServer = process.env.ID_SERVER;
 
-	
+
 client.on('message', message => {
 	if (message.content === '!comandos') {
-    	message.reply(lista);
+		message.reply(lista);
 	}
 	if (message.content === '!esteCanal') {
 		message.reply(message.channel.id);
@@ -21,23 +21,23 @@ client.on('message', message => {
 	if (message.content.startsWith('!play')) {
 		let videoUrl = message.content.split(" ");
 		console.log(videoUrl[1]);
-		/*const voiceChannel = message.member.voiceChannel;
-		if (!voiceChannel) return message.reply("Para poner un vídeo debes estar en un canal de voz");
-			voiceChannel.join()
-		  	.then(connnection => {
-			const stream = ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ", { filter: 'audioonly' });;
-			const dispatcher = connnection.playStream(stream);
-			dispatcher.on('end', () => voiceChannel.leave());
-		  });
-		  */
+		const voiceChannel = message.member.voiceChannel;
+		if (!voiceChannel) {
+			return message.reply("Para poner un vídeo debes estar en un canal de voz");
+		} else {
+			message.reply("Reproduciendo " + videoUrl[1]);
+		}
+		voiceChannel.join()
+			.then(connnection => {
+				const stream = ytdl(videoUrl[1], {
+					filter: 'audioonly'
+				});;
+				const dispatcher = connnection.playStream(stream);
+				dispatcher.on('end', () => voiceChannel.leave());
+			});
+
 	}
 });
-let videoID = "http://www.youtube.com/watch?v=A02s8omM_hI"
-ytdl.getInfo(videoID, (err, info) => {
-	if (err) throw err;
-	var audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-	console.log('Formats with only audio: ' + audioFormats.length)
-  })
 
 // Proceso de login inicial del Bot, imprescindible para su funcionamiento AL FINAL DEL FICHERO
 client.login(process.env.BOT_TOKEN);
